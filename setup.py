e22e26101fd02ee112046bd8012e8be6e38933ec
Víctor Molina García <victor@goa.uva.es>
2024-01-12 10:28:30 +0100

    Bump version number to 1.0.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ kwargs = {
     "name":
         "solo",
     "version":
-        "1.0.1",
+        "1.0.2",
     "license":
         "GNU General Public License v2 or later (GPLv2+)",
     "description":
--- solo/__init__.py
+++ solo/__init__.py
@@ -1,3 +1,3 @@
 from . radtran import radtran
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
